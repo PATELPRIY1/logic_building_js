b5ce0e8369b9f442ecec5311565cc981eca68e41
PATEL PRIYA <patelpriya380026@gamil.com>
2025-03-17 20:41:43 +0530

    first commit

---
+++ problem-23.js
@@ -0,0 +1,18 @@
+
+function numberPyramid(num){
+  for(let i = 1; i <= num; i++){
+    let lineContent = "";
+    for(let space = 1; space <= num - i; space++){
+      lineContent = lineContent + " ";
+    }
+    for(let count = 1; count <= i; count++){
+      lineContent = lineContent + count;
+    }
+    for(let reverseCount = i - 1; reverseCount >=1; reverseCount --){
+      lineContent = lineContent + reverseCount;
+    }
+    console.log(lineContent)
+  }
+}
+
+console.log(numberPyramid(10))
